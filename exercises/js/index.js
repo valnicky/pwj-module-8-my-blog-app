a7bcc1f1e67d86d0a6e92f7a261a981aed22c2ff
Valeria <valery0007@gmail.com>
2020-11-20 18:19:00 +0100

    fetch data

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -1,15 +1,22 @@
-
-
 const API_URL = "http://localhost:3000/api/posts";
 const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
-    getPosts();
-}
+  getPosts();
+};
 
 const getPosts = () => {
-}
+  fetch(API_URL, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      buildPosts(data);
+    });
+};
 
 const buildPosts = (blogPosts) => {
-
-}
+  console.log(blogPosts);
+};
