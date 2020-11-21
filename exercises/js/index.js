14b4ab6a36d4c6ae2d48c33196f5dff107072918
Valeria <valery0007@gmail.com>
2020-11-21 15:45:22 +0100

    buildPosts

--- exercises/js/index.js
+++ exercises/js/index.js
@@ -16,5 +16,19 @@ const getPosts = () => {
 };
 
 const buildPosts = (blogPosts) => {
-  console.log(blogPosts);
+  let blogPostsContent = "";
+  for(blogPost of blogPosts){
+    const postDate = new Date(parseInt(blogPost.added_date)).toDateString();
+    blogPostsContent += `<div class="post">
+                <div class="post-image"><img src="" alt=""></div>
+                <div class="post-content">
+                    <p class="post-date">${postDate}</p>
+                    <h4 class="post-title">${blogPost.title}</h4>
+                    <p class="post-text">${blogPost.content}
+                    </p>
+                </div>
+            </div>`
+  }
+  
+  document.querySelector('.blog-post').innerHTML = blogPostContent;
 };
